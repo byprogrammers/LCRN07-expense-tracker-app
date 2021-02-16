a07b2f20217fc6fe5acaba3daa8f94aac13e2408
Vincent <booncheng89@gmail.com>
2021-02-16 14:47:37 +0800

    Fix Pie Chart slice onPress issue

--- screens/Home.js
+++ screens/Home.js
@@ -10,9 +10,12 @@ import {
     ImageBackground,
     TouchableOpacity,
     FlatList,
-    Animated
+    Animated,
+    Platform
 } from 'react-native';
 import { VictoryPie } from 'victory-native';
+
+import {Svg} from 'react-native-svg';
 
 import { COLORS, FONTS, SIZES, icons, images } from '../constants';
 
@@ -605,47 +608,99 @@ const Home = () => {
         console.log("Check Chart")
         console.log(chartData)
 
-        return (
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <VictoryPie
-                    data={chartData}
-                    labels={(datum) => `${datum.y}`}
-                    radius={({ datum }) => (selectedCategory && selectedCategory.name == datum.name) ? SIZES.width * 0.4 : SIZES.width * 0.4 - 10}
-                    innerRadius={70}
-                    labelRadius={({ innerRadius }) => (SIZES.width * 0.4 + innerRadius) / 2.5}
-                    style={{
-                        labels: { fill: "white", ...FONTS.body3 },
-                        parent: {
-                            ...styles.shadow
-                        },
-                    }}
-                    width={SIZES.width * 0.8}
-                    height={SIZES.width * 0.8}
-                    colorScale={colorScales}
-                    events={[{
-                        target: "data",
-                        eventHandlers: {
-                            onPress: () => {
-                                return [{
-                                    target: "labels",
-                                    mutation: (props) => {
-                                        let categoryName = chartData[props.index].name
-                                        setSelectCategoryByName(categoryName)
-                                    }
-                                }]
+        if(Platform.OS == 'ios')
+        {
+            return (
+                <View  style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <VictoryPie
+                        
+                        data={chartData}
+                        labels={(datum) => `${datum.y}`}
+                        radius={({ datum }) => (selectedCategory && selectedCategory.name == datum.name) ? SIZES.width * 0.4 : SIZES.width * 0.4 - 10}
+                        innerRadius={70}
+                        labelRadius={({ innerRadius }) => (SIZES.width * 0.4 + innerRadius) / 2.5}
+                        style={{
+                            labels: { fill: "white", ...FONTS.body3 },
+                            parent: {
+                                ...styles.shadow
+                            },
+                        }}
+                        width={SIZES.width * 0.8}
+                        height={SIZES.width * 0.8}
+                        colorScale={colorScales}
+                        events={[{
+                            target: "data",
+                            eventHandlers: {
+                                onPress: () => {
+                                    return [{
+                                        target: "labels",
+                                        mutation: (props) => {
+                                            let categoryName = chartData[props.index].name
+                                            setSelectCategoryByName(categoryName)
+                                        }
+                                    }]
+                                }
                             }
-                        }
-                    }]}
-
-                />
-
-                <View style={{ position: 'absolute', top: '42%', left: "42%" }}>
-                    <Text style={{ ...FONTS.h1, textAlign: 'center' }}>{totalExpenseCount}</Text>
-                    <Text style={{ ...FONTS.body3, textAlign: 'center' }}>Expenses</Text>
+                        }]}
+    
+                    />
+    
+                    <View style={{ position: 'absolute', top: '42%', left: "42%" }}>
+                        <Text style={{ ...FONTS.h1, textAlign: 'center' }}>{totalExpenseCount}</Text>
+                        <Text style={{ ...FONTS.body3, textAlign: 'center' }}>Expenses</Text>
+                    </View>
                 </View>
-            </View>
+    
+            )
+        }
+        else
+        {
+            // Android workaround by wrapping VictoryPie with SVG
+            return (
+                <View  style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Svg width={SIZES.width} height={SIZES.width} style={{width: "100%", height: "auto"}}>
 
-        )
+                        <VictoryPie
+                            standalone={false} // Android workaround
+                            data={chartData}
+                            labels={(datum) => `${datum.y}`}
+                            radius={({ datum }) => (selectedCategory && selectedCategory.name == datum.name) ? SIZES.width * 0.4 : SIZES.width * 0.4 - 10}
+                            innerRadius={70}
+                            labelRadius={({ innerRadius }) => (SIZES.width * 0.4 + innerRadius) / 2.5}
+                            style={{
+                                labels: { fill: "white", ...FONTS.body3 },
+                                parent: {
+                                    ...styles.shadow
+                                },
+                            }}
+                            width={SIZES.width}
+                            height={SIZES.width}
+                            colorScale={colorScales}
+                            events={[{
+                                target: "data",
+                                eventHandlers: {
+                                    onPress: () => {
+                                        return [{
+                                            target: "labels",
+                                            mutation: (props) => {
+                                                let categoryName = chartData[props.index].name
+                                                setSelectCategoryByName(categoryName)
+                                            }
+                                        }]
+                                    }
+                                }
+                            }]}
+        
+                        />
+                    </Svg>
+                    <View style={{ position: 'absolute', top: '42%', left: "42%" }}>
+                        <Text style={{ ...FONTS.h1, textAlign: 'center' }}>{totalExpenseCount}</Text>
+                        <Text style={{ ...FONTS.body3, textAlign: 'center' }}>Expenses</Text>
+                    </View>
+                </View>
+            )
+        }
+        
     }
 
     function renderExpenseSummary() {
